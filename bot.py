23c308052b4bb3f8ea34e4440ef80e3ac768ce8b
Edmund Lin <elz.work22@gmail.com>
2024-06-11 19:45:52 +0800

    chore: remove dotenv

--- bot.py
+++ bot.py
@@ -1,16 +1,29 @@
 import os
-from dotenv import load_dotenv
-from telegram.ext import Updater, CommandHandler
+import logging
+from telegram.ext import MessageHandler, Updater, CommandHandler, Filters
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def start(update, context):
     update.message.reply_text('Hello! I am Mighty Macaque!')
 
+def handle_message(update, context):
+    text = update.message.text.lower()
+    if 'hi' in text:
+        update.message.reply_text('Hello there!')
+
 def main():
-    load_dotenv()
     TOKEN = os.getenv('API_KEY')
+    if not TOKEN:
+        logger.error('No API_KEY found in environment variables')
+        return
     updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
     updater.start_polling()   
     updater.idle()
 
